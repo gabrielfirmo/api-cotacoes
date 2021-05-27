9f7d432145e52c7e4f191b7be61ecfa3379fcb84
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-05-26 21:48:49 -0300

    Adiciona validação dos dados

--- src/main/java/com/santander/bootcamp/model/dto/StockDTO.java
+++ src/main/java/com/santander/bootcamp/model/dto/StockDTO.java
@@ -1,9 +1,12 @@
 package com.santander.bootcamp.model.dto;
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Getter
@@ -11,8 +14,20 @@ import java.time.LocalDate;
 public class StockDTO {
 
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
+    @DecimalMin("0.00")
+    @Digits(integer = 6, fraction = 2)
     private Double price;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate date;
+
+    @NotNull
+    @Digits(integer = 3, fraction = 2)
     private Double variation;
 }
