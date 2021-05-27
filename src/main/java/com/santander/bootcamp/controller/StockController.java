7027df383ad8674fefea759dc78b878a4d4f9125
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-05-26 21:25:57 -0300

    Adiciona o OpenAPI

--- src/main/java/com/santander/bootcamp/controller/StockController.java
+++ src/main/java/com/santander/bootcamp/controller/StockController.java
@@ -1,2 +1,65 @@
-package com.santander.bootcamp.controller;public class StockController {
+package com.santander.bootcamp.controller;
+
+import com.santander.bootcamp.model.dto.StockDTO;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> save(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> update(@RequestBody StockDTO dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDTO>> findAll(){
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Magazine Luiza");
+        dto.setPrice(100D);
+        dto.setVariation(10D);
+        dto.setDate(LocalDate.now());
+
+        list.add(dto);
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDTO> findById(@PathVariable Long id){
+        List<StockDTO> list = new ArrayList<>();
+        StockDTO dto = new StockDTO();
+        dto.setId(1L);
+        dto.setName("Magazine Luiza");
+        dto.setPrice(100D);
+        dto.setVariation(10D);
+        dto.setDate(LocalDate.now());
+
+        StockDTO dto2 = new StockDTO();
+        dto2.setId(2L);
+        dto2.setName("Ponto Frio");
+        dto2.setPrice(200D);
+        dto2.setVariation(5D);
+        dto2.setDate(LocalDate.now());
+
+        list.add(dto);
+        list.add(dto2);
+
+        StockDTO selected = list.stream().filter(x -> x.getId().equals(id)).findFirst().get();
+
+        return ResponseEntity.ok(selected);
+    }
+
 }
--- src/main/java/com/santander/bootcamp/model/dto/StockDTO.java
+++ src/main/java/com/santander/bootcamp/model/dto/StockDTO.java
@@ -1,4 +1,18 @@
 package com.santander.bootcamp.model.dto;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class StockDTO {
+
+    private Long id;
+    private String name;
+    private Double price;
+    private LocalDate date;
+    private Double variation;
 }
